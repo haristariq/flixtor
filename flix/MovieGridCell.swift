2b9962968aafc1de64256570ba204212690646c4
haristariq <72283839+haristariq@users.noreply.github.com>
2020-10-30 23:10:24 -0700

    Add files via upload

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Mayur Barge on 2/14/19.
+//  Copyright © 2019 Mayur Barge. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
